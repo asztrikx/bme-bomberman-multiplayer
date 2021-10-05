88f7cc23d8659187d7fd2933b2ec8930ca840ee7
asztrikx <asztrik35@gmail.com>
2021-10-05 12:04:42 +0200

    fixed speeds

--- helper/Config.java
+++ helper/Config.java
@@ -1,9 +1,10 @@
 package helper;
 
 public class Config {
+	// TODO should be same between server and client
 	public int squaresize = 50;
-	public int velocityPlayer = 6;
-	public int velocityEnemy = 2;
+	public int velocityPlayer = 3;
+	public int velocityEnemy = 1;
 	// TODO make position etc for others, optimize after like offset variable
 	public int windowHeight = 480;
 	public int windowWidth = 640;
@@ -11,7 +12,7 @@ public class Config {
 	public int worldWidth = 13;
 	public double boxRatio = 0.25;
 	public double enemyRatio = 0.05;
-	public double enemyKeyChangePossibility = 0.015;
+	public double enemyKeyChangePossibility = 0.0015;
 	public long tickRate = 1000 / 58;
 	public long tickSecond = 1000 / tickRate; // tick count in one second
 	public int authLength = 26;

e46eaa349e9e3e9690e52946f52faa6c5bda70bb
asztrikx <asztrik35@gmail.com>
2021-11-26 17:15:30 +0100

    reverting Flag using logger

--- flag/Flag.java
+++ flag/Flag.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import di.DI;
-import helper.Logger;
-
+/**
+ * @formatter:off
+ * Logger should not be used here as
+ * - it is specific for CLI
+ * - logger requires DI to be inited which requires config file which is read from cli (here)
+ * @formatter:on
+ */
 public class Flag {
-	private static Logger logger = (Logger) DI.get(Logger.class);
-
 	Map<String, Flag.Entry> commands;
 
 	public Flag(final Map<String, Flag.Entry> commands) {
@@ -32,9 +34,9 @@ public class Flag {
 	}
 
 	public void printHelp() {
-		logger.println("Available commands");
+		System.out.println("Available commands");
 		commands.forEach((final String name, final Entry entry) -> {
-			logger.println(String.format("%s: %s", name, entry.helpMessage));
+			System.out.println(String.format("%s: %s", name, entry.helpMessage));
 		});
 	}
 
@@ -50,13 +52,13 @@ public class Flag {
 		for (int i = 0; i < args.length; i++) {
 			final String arg = args[i];
 			if (!commands.containsKey(arg)) {
-				logger.println(String.format("Unknown command: %s", arg));
+				System.out.println(String.format("Unknown command: %s", arg));
 				return Optional.empty();
 			}
 
 			final Entry entry = commands.get(arg);
 			if (!entry.noParam && i + 1 == args.length) {
-				logger.println(String.format("Missing parameter for: %s", arg));
+				System.out.println(String.format("Missing parameter for: %s", arg));
 				return Optional.empty();
 			}
 
@@ -79,11 +81,12 @@ public class Flag {
 			if (!parsed.containsKey(name)) {
 				if (entry.defaultValue == null) {
 					if (entry.required) {
-						logger.println(String.format("Missing required parameter: %s, %s", name, entry.helpMessage));
+						System.out
+								.println(String.format("Missing required parameter: %s, %s", name, entry.helpMessage));
 						missing = true;
 					}
 				} else {
-					logger.println(String.format("Using default value %s for %s", entry.defaultValue, name));
+					System.out.println(String.format("Using default value %s for %s", entry.defaultValue, name));
 					parsed.put(name, entry.defaultValue);
 				}
 			}
@@ -99,7 +102,7 @@ public class Flag {
 		boolean missing = false;
 		for (final String requiredName : requiredNames) {
 			if (!parsed.containsKey(requiredName)) {
-				logger.println(String.format("Missing required parameter: %s, %s", requiredName,
+				System.out.println(String.format("Missing required parameter: %s, %s", requiredName,
 						commands.get(requiredName).helpMessage));
 				missing = true;
 			}

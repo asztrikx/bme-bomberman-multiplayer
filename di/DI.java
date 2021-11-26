0ac1d55ac939b250cc7182c2ebc73e309b056616
asztrikx <asztrik35@gmail.com>
2021-11-26 14:56:36 +0100

    moved DI init into DI

--- di/DI.java
+++ di/DI.java
@@ -1,7 +1,12 @@
 package di;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import helper.Config;
+import helper.Logger;
+import world.element.AnimationStore;
 
 public class DI {
 	public static Map<Class<? extends Object>, Object> services = new HashMap<>();
@@ -14,5 +19,20 @@ public class DI {
 	 */
 	public static void put(final Object object) {
 		services.put(object.getClass(), object);
+	}
+
+	public static void init() {
+		Logger logger = new Logger(System.out);
+		Config config;
+		try {
+			config = Config.getConfig();
+		} catch (IOException e) {
+			logger.printf("Could not read %s. Please try to fix this by deleting the file\n", Config.configFileName);
+			return;
+		}
+
+		DI.put(config);
+		DI.put(logger);
+		DI.put(new AnimationStore());
 	}
 }
